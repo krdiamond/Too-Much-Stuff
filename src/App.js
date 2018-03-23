75b98cc0dc3fad80a15ef28feedf885004dd6008
cnlucke <cristy.lucke@gmail.com>
2018-03-22 21:27:30 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <button className="button">START</button>
+      </div>
+    );
+  }
+}
+
+export default App;
