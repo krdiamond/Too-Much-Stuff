d2077a79db6e34075003ba6afb150dc29549f155
krdiamond <github email address>
2018-03-24 12:01:19 -0400

    added css to make the items stay in on the trash pile

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,26 +6,18 @@ export default class Game extends Component {
     imgs: []
   }
 
-<<<<<<< HEAD
-  // componentDidMount() {
-  //   fetch('http://localhost:3000/items')
-  //   .then(res => res.json())
-  //   .then(json => this.setState({imgs: json}))
-  // }
-=======
   componentDidMount() {
     fetch('http://localhost:3000/items')
     .then(res => res.json())
     .then(json => this.setState({imgs: json}, () => console.log("stage imgs after fetch", this.state.imgs)))
   }
->>>>>>> 276f4afd78a0326d97a7f33b02b2408ccb45d76c
 
   render() {
     console.log("Got to game!")
     return(
       <div className="image_container">
         <div className="main_image"> <img src={TrashMountain} height="900" alt="Trash Mountain"/> </div>
-        <div>
+        <div className="item">
           {this.state.imgs ? this.state.imgs.map((img) => {
             return (
               <img src={img.src} style={{height: '40px', width: '40px'}}/>
