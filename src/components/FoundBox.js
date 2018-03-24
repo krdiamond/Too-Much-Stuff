19a9ac7e9a355f70c5a91f5b89cb3ff0883f044a
cnlucke <cristy.lucke@gmail.com>
2018-03-24 12:43:03 -0400

    got rid of console warnings and errors and unneeded logging

--- src/components/FoundBox.js
+++ src/components/FoundBox.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ItemList from './ItemList'
 
 const FoundBox = (props) => {
-  console.log("FoundBox Props:", props)
   return(
     <div className="temp" style={{height: 200,
                                   width: 200,
                                   border: '2px solid red',
                                   display: 'inline-block',
-                                  "text-align": 'center'}}>
+                                  textAlign: 'center'}}>
       {"FOUND ITEMS"}
       {/* NOTE: this handleClick will need to get to the div holding the items some other way eventually */}
       <ItemList list={props.found} handleClick={props.handleClick} />
