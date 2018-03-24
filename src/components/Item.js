aee333ed284e5bb70b172124c0069ab84f00e921
krdiamond <github email address>
2018-03-24 14:51:51 -0400

    items are ontop of the trash pile

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 const Item = (props) => {
   return(
-    <span className="item">
-      <img  src={props.img.src}
-            onClick={() => props.handleClick(props.img)}
-            style={{height: '30px', width: '30px'}}
-            alt={props.img.name}/>
-    </span>
+      <img height="30" width="30" src={props.img.src} onClick={() => props.handleClick(props.img)} alt={props.img.name}/>
   )
 }
 
