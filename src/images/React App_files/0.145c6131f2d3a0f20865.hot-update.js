43faea307d8c7ec596b98604f7daec932bde6968
krdiamond <github email address>
2018-04-15 10:23:59 -0400

    added speech bubbles to room 1

---
+++ src/images/React App_files/0.145c6131f2d3a0f20865.hot-update.js
@@ -0,0 +1,24 @@
+webpackHotUpdate(0,{
+
+/***/ "./node_modules/css-loader/index.js?{\"importLoaders\":1}!./node_modules/postcss-loader/lib/index.js?{\"ident\":\"postcss\",\"plugins\":[null,null]}!./src/components/game_play/rooms/room7/Room7.css":
+/*!**********************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader?{"importLoaders":1}!./node_modules/postcss-loader/lib?{"ident":"postcss","plugins":[null,null]}!./src/components/game_play/rooms/room7/Room7.css ***!
+  \**********************************************************************************************************************************************************************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../../../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(undefined);
+// imports
+
+
+// module
+exports.push([module.i, "/* -------------------------- NAVIGATE TO ROOM 6 ---------------------------- */\n\n#room7_go_to_room_6{\n  position: absolute;\n  top: 380px;\n  left: 10px;\n  width: 180px;\n  height: 250px;\n  /* border: 6px solid purple; */\n}\n\n#room7_go_to_room_6:hover{\n  cursor: url(" + __webpack_require__(/*! ../../../../images/arrows/left_arrow.png */ "./src/images/arrows/left_arrow.png") + ") 10 30,auto;\n}\n\n #room7_go_to_room_6_text {\n  width: 100px;\n  visibility: hidden;\n  background-color: black;\n  color: #fff;\n  border-radius: 6px;\n  padding: 5px 0;\n }\n\n #room7_go_to_room_6:hover #room7_go_to_room_6_text  {\n   visibility: visible;\n }\n\n/* -------------------------- NAVIGATE TO ROOM 6 ---------------------------- */\n\n#room7_go_to_room_8{\n  position: absolute;\n  bottom: 0px;\n  left: 0px;\n  width: 280px;\n  height: 250px;\n  /* border: 6px solid purple; */\n}\n\n#room7_go_to_room_8:hover{\n  cursor: url(" + __webpack_require__(/*! ../../../../images/arrows/diagonal_left_up_arrow.png */ "./src/images/arrows/diagonal_left_up_arrow.png") + ") 30 30,auto;\n}\n\n #room7_go_to_room_8_text {\n  width: 100px;\n  visibility: hidden;\n  background-color: black;\n  color: #fff;\n  border-radius: 6px;\n  padding: 5px 0;\n }\n\n #room7_go_to_room_8:hover #room7_go_to_room_8_text  {\n   visibility: visible;\n }\n\n/* -------------------------------- PHONE ----------------------------------- */\n\n#room7phone {\n  position: absolute;\n  width: 90px;\n  height: 50px;\n  top: 809px;\n  left: 302px;\n  /* border: 3px solid green; */\n}\n\n/* --------------------------------- NOTE ----------------------------------- */\n\n#fridge_note {\n  position: absolute;\n  top: 312px;\n  left: 30px;\n}\n\n#beach_club_address {\n  position: absolute;\n  /* border: 6px solid purple; */\n  height:200px;\n  width: 200px;\n  top: 200px;\n  left: 580px;\n  z-index: 3;\n    background-color:white;\n}\n", ""]);
+
+// exports
+
+
+/***/ })
+
+})
+//# sourceMappingURL=0.145c6131f2d3a0f20865.hot-update.js.map
